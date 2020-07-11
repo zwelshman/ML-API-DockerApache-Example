9f0edd3d4504897fea9478bc795c90dcd7e0190b
Your Namezwelshman <zmswelshman@gmail.com>
2020-07-11 14:44:20 +0100

    inital commit

---
+++ flask_demo/flask_predict_train.py
@@ -0,0 +1,54 @@
+# Random Forest Classifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+import pandas as pd
+
+# loading the dataset
+iris = load_iris()
+X = iris.data
+y = iris.target
+
+# Split the data
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, test_size=0.7)
+
+# Build the model
+clf = RandomForestClassifier(n_estimators=10)
+
+# Train the classifier
+clf.fit(X_train, y_train)
+
+# Predictions
+predicted = clf.predict(X_test)
+
+# Check accuracy
+print(accuracy_score(predicted, y_test))
+
+import pickle
+with open('./rf.pkl', 'wb') as model_pkl:
+    pickle.dump(clf, model_pkl, protocol=2)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
